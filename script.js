5894a214e62434198ed7598311bdbb2c40c7b55d
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-07-19 14:00:40 -0400

    added animated navbar script for mobile nav

---
+++ script.js
@@ -0,0 +1,29 @@
+const mobileNav = document.querySelector(".mobileNav");
+mobileNav.onclick = () => {
+    const nav = document.querySelector(".navContainer");
+    const bar1 = document.querySelector(".bar1");
+    const bar2 = document.querySelector(".bar2");
+    const bar3 = document.querySelector(".bar3");
+    //reset all
+    if (nav.style.top === "0px") {
+        nav.style.top = "-1500%";
+        bar1.style.transform = "rotate(0)";
+        bar1.style.backgroundColor = "black";
+        bar1.style.borderColor = "white";
+        bar2.style.opacity = "1";
+        bar3.style.transform = "rotate(0)";
+        bar3.style.backgroundColor = 'black';
+        bar3.style.borderColor = 'white';
+    }
+    //lower nav bar and change mobile nav
+    else {
+        nav.style.top = "0";
+        bar1.style.transform = "rotate(-45deg) translateY(14px)";
+        bar1.style.backgroundColor = "#ae0c08";
+        bar1.style.borderColor = "#ae0c08";
+        bar2.style.opacity = "0";
+        bar3.style.transform = "rotate(45deg) translateY(-14px)";
+        bar3.style.backgroundColor = '#ae0c08';
+        bar3.style.borderColor = '#ae0c08';
+    }
+};
