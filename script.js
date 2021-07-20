69f60e4092c97806ae92ce14874f0ffcb618abda
GsSteven <65253485+GsSteven@users.noreply.github.com>
2021-07-20 17:31:42 -0400

    added jquery for mobile nav and expanding watch details

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-const mobileNav = document.querySelector(".mobileNav");
-mobileNav.onclick = () => {
-    const nav = document.querySelector(".navContainer");
-    const bar1 = document.querySelector(".bar1");
-    const bar2 = document.querySelector(".bar2");
-    const bar3 = document.querySelector(".bar3");
-    //reset all
-    if (nav.style.top === "0px") {
-        nav.style.top = "-1500%";
-        bar1.style.transform = "rotate(0)";
-        bar1.style.backgroundColor = "white";
-        bar1.style.borderColor = "black";
-        bar2.style.opacity = "1";
-        bar3.style.transform = "rotate(0)";
-        bar3.style.backgroundColor = 'white';
-        bar3.style.borderColor = 'black';
-    }
-    //lower nav bar and change mobile nav
-    else {
-        nav.style.top = "0";
-        bar1.style.transform = "rotate(-45deg) translateY(14px)";
-        bar1.style.backgroundColor = "#ae0c08";
-        bar1.style.borderColor = "#ae0c08";
-        bar2.style.opacity = "0";
-        bar3.style.transform = "rotate(45deg) translateY(-14px)";
-        bar3.style.backgroundColor = '#ae0c08';
-        bar3.style.borderColor = '#ae0c08';
-    }
-};
+$("document").ready(() => {
+    //mobile Nav animations
+    $(".mobileNav").click(() => {
+        if ($('.navContainer').css("top") === "0px") {
+            //slide nav up
+            $('.navContainer').css("top", "-1500%");
+            //reset mobile nav styles
+            $('.bar1').css({ "transform": "rotate(0)", "backgroundColor": "white", "borderColor": "black" });
+            $('.bar2').css("opacity", "1");
+            $('.bar3').css({ "transform": "rotate(0)", "backgroundColor": "white", "borderColor": "black" });
+        } else {
+            //slide nav down
+            $('.navContainer').css("top", "0");
+            //mobile icon turns into X
+            $('.bar1').css({ "transform": "rotate(-45deg) translateY(14px)", "backgroundColor": "#ae0c08", "borderColor": "#ae0c08" });
+            $('.bar2').css("opacity", "0");
+            $('.bar3').css({ "transform": "rotate(45deg) translateY(-14px)", "backgroundColor": "#ae0c08", "borderColor": "#ae0c08" });
+        }
+    })
+    //open expanded Watch Faces
+    $(".watchFaceContainer").click((e) => {
+        const expandedContainerId = e.target.id + "Expanded";
+        if (expandedContainerId) {
+            $(`#${expandedContainerId}`).css("display", "flex");
+        }
+    })
+    //close expanded Watch Faces
+    $(".closeButton").click(() => {
+        $(".closeButton").parent().css("display", "none");
+    });
+});
